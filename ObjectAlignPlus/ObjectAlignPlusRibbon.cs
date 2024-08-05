ae24fdc1564f22711d30c0112842e59b0eb0d41e
dnasoftwares <dna@dna-softwares.com>
2024-08-05 12:28:46 +0900

    回転済みシェイプにもある程度対応
    シェイプの外接矩形で計算してるので、矩形以外だと不自然な結果になる場合あり

--- ObjectAlignPlus/ObjectAlignPlusRibbon.cs
+++ ObjectAlignPlus/ObjectAlignPlusRibbon.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Office.Tools.Ribbon;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using Microsoft.Office.Interop.PowerPoint;
 
 namespace ObjectAlignPlus
@@ -68,6 +72,51 @@ namespace ObjectAlignPlus
 
             return shapeList;
         }
+
+        private RectangleF GetShapeExactRect(Shape shape)
+        {
+            //Shapeの位置、サイズ、回転から正確な外形矩形を得る
+            //頂点は基準点から反時計回り
+            var verts = new PointF[4];
+            verts[0] = new PointF(shape.Left, shape.Top);
+            verts[1] = new PointF(shape.Left + shape.Width, shape.Top);
+            verts[2] = new PointF(shape.Left + shape.Width, shape.Top + shape.Height);
+            verts[3] = new PointF(shape.Left, shape.Top + shape.Height);
+
+            //Shapeの回転角度を取得 (単位は度)
+            var angle = shape.Rotation;
+            //色々都合が良いのでラジアンにしておく
+            var rad = angle * System.Math.PI / 180;
+
+            var rotcenter = new PointF(shape.Left + shape.Width / 2, shape.Top + shape.Height / 2);
+
+            //0,90,180,270度の場合は三角関数を使わずに計算
+            if (angle == 0 || angle==180)
+            {
+                return new RectangleF(verts[0], new SizeF(shape.Width, shape.Height));
+            }
+            else if (angle == 90 || angle==270)
+            {
+                return new RectangleF(new PointF(rotcenter.X-shape.Height/2,rotcenter.Y-shape.Width/2),
+                    new SizeF(shape.Height, shape.Width));
+            }
+
+            //回転中心を原点にして、頂点を回転させる
+            for (int i = 0; i < 4; i++)
+            {
+                var x = verts[i].X - rotcenter.X;
+                var y = verts[i].Y - rotcenter.Y;
+                verts[i].X = (float)(x * System.Math.Cos(rad) - y * System.Math.Sin(rad) + rotcenter.X);
+                verts[i].Y = (float)(x * System.Math.Sin(rad) + y * System.Math.Cos(rad) + rotcenter.Y);
+            }
+            //頂点座標が出たらそれらの座標から外接矩形を求める
+            var left = verts.Select(v => v.X).Min();
+            var top = verts.Select(v => v.Y).Min();
+            var right = verts.Select(v => v.X).Max();
+            var bottom = verts.Select(v => v.Y).Max();
+            return new RectangleF(left, top, right - left, bottom - top);
+        }
+
         private void btHorz_Click(object sender, RibbonControlEventArgs e)
         {
             var shapeList = GetSelectedShape();
@@ -79,14 +128,19 @@ namespace ObjectAlignPlus
             int spacing = validateSpacingText();
 
             //X座標で左から順にソート
-            shapeList.Sort((a, b) => a.Left.CompareTo(b.Left));
+            shapeList.Sort((a, b) =>
+                GetShapeExactRect(a).Left.CompareTo(GetShapeExactRect(b).Left));
 
-            //左端＝[0]のオブジェクトは基準なので動かさない。[0]の左端＋[0]の幅＋spacingからはじめる
-            var x = shapeList[0].Left + shapeList[0].Width + spacing;
+            var rect0 = GetShapeExactRect(shapeList[0]);
+
+            //左端＝[0]のオブジェクトは基準なので動かさない。[0]の右端＋spacingからはじめる
+            var x = rect0.Right + spacing;
             for(int i = 1; i < shapeList.Count; i++)
             {
-                shapeList[i].Left = x;
-                x += shapeList[i].Width + spacing;
+                var rect = GetShapeExactRect(shapeList[i]);
+                //ShapeのLeftと外接矩形のLeftが違うので、外接矩形のLeft同士で差分を取って平行移動
+                shapeList[i].IncrementLeft(x-rect.Left);
+                x += rect.Width + spacing;
             }
         }
 
@@ -102,14 +156,19 @@ namespace ObjectAlignPlus
             int spacing = validateSpacingText();
 
             //X座標で右から順にソート
-            shapeList.Sort((a, b) => (b.Left+b.Width).CompareTo(a.Left+a.Width));
+            shapeList.Sort((a, b) =>
+                GetShapeExactRect(b).Right.CompareTo(GetShapeExactRect(a).Right)
+            );
 
-            //右端＝[0]のオブジェクトは基準なので動かさない。[0]の右端-spacingからはじめる
-            var x = shapeList[0].Left;
+            //右端＝[0]のオブジェクトは基準なので動かさない。[0]の左端＋spacingからはじめる
+            var rect0 = GetShapeExactRect(shapeList[0]);
+
+            var x = rect0.Left - spacing;
             for (int i = 1; i < shapeList.Count; i++)
             {
-                x -= shapeList[i].Width + spacing;
-                shapeList[i].Left = x;
+                var rect = GetShapeExactRect(shapeList[i]);
+                shapeList[i].IncrementLeft(x - rect.Right);
+                x -= rect.Width + spacing;
             }
         }
 
@@ -123,13 +182,15 @@ namespace ObjectAlignPlus
             int spacing = validateSpacingText();
 
             //Y座標で上から順にソート
-            shapeList.Sort((a, b) => a.Top.CompareTo(b.Top));
-            var x = shapeList[0].Top+shapeList[0].Height+spacing;
-            //上端＝[0]のオブジェクトは基準なので動かさない。[0]の上端＋[0]の高さ＋spacingからはじめる
+            shapeList.Sort((a, b) => GetShapeExactRect(a).Top.CompareTo(GetShapeExactRect(b).Top));
+
+            //上端＝[0]のオブジェクトは基準なので動かさない。[0]の下端+spacingからはじめる
+            var y = GetShapeExactRect(shapeList[0]).Bottom + spacing;
             for (int i = 1; i < shapeList.Count; i++)
             {
-                shapeList[i].Top = x;
-                x += shapeList[i].Height + spacing;
+                var rect = GetShapeExactRect(shapeList[i]);
+                shapeList[i].IncrementTop(y- rect.Top);
+                y += rect.Height + spacing;
             }
         }
 
@@ -143,14 +204,16 @@ namespace ObjectAlignPlus
             int spacing = validateSpacingText();
 
             //Y座標で下から順にソート
-            shapeList.Sort((a, b) => (b.Top + b.Height).CompareTo(a.Top + a.Height));
+            shapeList.Sort((a, b) => GetShapeExactRect(b).Bottom.CompareTo(GetShapeExactRect(a).Bottom));
 
-            //下端＝[0]のオブジェクトは基準なので動かさない。[0]の下端-spacingからはじめる
-            var x = shapeList[0].Top;
+            //下端＝[0]のオブジェクトは基準なので動かさない。[0]の上端-spacingからはじめる
+            var rect0 = GetShapeExactRect(shapeList[0]);
+            var y = rect0.Top - spacing;
             for (int i = 1; i < shapeList.Count; i++)
             {
-                x -= shapeList[i].Height + spacing;
-                shapeList[i].Top = x;
+                var rect = GetShapeExactRect(shapeList[i]);
+                shapeList[i].IncrementTop(y - rect.Bottom);
+                y -= rect.Height + spacing;
             }
         }
     }
